2116265dc32bb5b7adc59d5bef05dd0d5fb8d129
rocioacaseres <51235147+rocioacaseres@users.noreply.github.com>
2023-06-14 20:22:56 -0300

    Update bingo.sh

--- bingo.sh
+++ bingo.sh
@@ -1,14 +1,24 @@
-echo $NUMBER
-if ((NUMBER <= 15))
-then 
-   echo $TEXT B:$NUMBER
+#!/bin/bash
+
+# Bingo Number Generator
+
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+NUMBER=$(( RANDOM % 75 + 1 ))
+TEXT="The next number is, "
+
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
 elif [[ $NUMBER -le 30 ]]
 then
-   echo $TEXT I:$NUMBER
-elif ((NUMBER < 46))
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
 then
   echo $TEXT N:$NUMBER
-elif [[ $NUMBER -l 46 ]]
+elif [[ $NUMBER -lt 61 ]]
 then
-   echo $TEXT G:$NUMBER
-fi  
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT O:$NUMBER
+fi
