2f7e80e73af2356787b12da58329827a0a2fa46f
rocioacaseres <51235147+rocioacaseres@users.noreply.github.com>
2023-06-14 00:29:31 -0300

    Update questionnaire.sh

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo -e "\n~~ Questionnaire ~~\n"
 QUESTION1="What's your name?"
 echo $QUESTION1
 read NAME
 echo Hello $NAME.
+QUESTION2="Where are you from?"
+echo $QUESTION2
+read LOCATION
+echo Hello $NAME from $LOCATION.
