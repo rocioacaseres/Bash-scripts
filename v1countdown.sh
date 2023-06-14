954ca15ac2ab08c005aa22142c184c6e81b80bd0
rocioacaseres <51235147+rocioacaseres@users.noreply.github.com>
2023-06-14 11:25:19 -0300

    Create v1countdown.sh

---
+++ v1countdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Program that counts down to zero from a given argument
+
+if [[ $1 == arg1 ]]
+then
+  echo true
+else
+  echo false
+fi
+#prints true only if the first argument is arg1
